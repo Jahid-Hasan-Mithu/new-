fa40008c2ec163136473f79bf9001579d23c2a82
Jahid-Hasan-Mithu <jahidhasan.jh111@gmail.com>
2020-10-09 00:54:27 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function() {
+
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
